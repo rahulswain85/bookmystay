a223592b9333f255f7485fdf851053766cea7963
rahulswain85 <rahul8033swain@gmail.com>
2025-09-11 22:09:21 +0530

    Error Handeled

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const methodOverride = require("method-override");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 const ejsmate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressErrors = require("./utils/ExpressErrors.js");
 
 main().then(()=>{console.log("connected to db")}).catch(err=>{console.log(err)});
+
 async function main(){
     await mongoose.connect(mongo_url);
 }
@@ -16,8 +19,6 @@ async function main(){
 const app = express();
 
 const port = 3000;
-
-app.listen(port, (req, res)=>{console.log(`listening to port 3000`)});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -31,66 +32,76 @@ app.engine("ejs", ejsmate);
 
 
 
-
+//Show All Listings
 app.get("/listings", async (req, res)=>{
     const allListing = await Listing.find({});
 
     res.render("./listings/index.ejs", {allListing});
 });
 
+//Add new listing form
 app.get("/listings/new", (req, res)=>{
     res.render("./listings/new.ejs");
 });
 
-app.get("/listings/:id/edit", async (req, res)=>{
-    let {id} = req.params;
+
+//Edit Form
+app.get("/listings/:id/edit",wrapAsync(async (req, res)=>{
     
-    const currListing = await Listing.findById(id);
-
-    res.render("./listings/edit.ejs", {currListing});
-});
-
-app.get("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
-
-    let listing = await Listing.findById(id);
-
-    res.render("./listings/show.ejs", {listing});
-})
-
-app.post("/listings", async (req, res) => {
-
-    const newListing = new Listing(req.body.listing);
+        let {id} = req.params;
+        const currListing = await Listing.findById(id);
+        res.render("./listings/edit.ejs", {currListing});
     
-    newListing.save().then(result=>{
-        console.log(result);
+}) );
+
+
+//Show Listing
+app.get("/listings/:id",wrapAsync(async (req, res)=>{
+
+        let {id} = req.params;
+        let listing = await Listing.findById(id);
+        res.render("./listings/show.ejs", {listing});
+    
+}));
+
+app.post("/listings",wrapAsync(async (req, res, next) => {
+
+    if(!req.body.listing)throw new ExpressErrors(400, "Bad Request!");
+
+        const newListing = new Listing(req.body.listing);
+    
+        await newListing.save();
         res.redirect("/listings");
-    }).catch(err => {console.log(err);
-        res.send("Failed to ADD");
-    });
+}));
 
-});
-
-app.put("/listings/:id", async(req, res)=>{
+app.put("/listings/:id",wrapAsync(async(req, res)=>{
+    if(!req.body.listing)throw new ExpressErrors(400, "Bad Request!");
     let {id} = req.params;
     let edited = req.body.listing;
 
-    Listing.findByIdAndUpdate(id, edited, {new: true}).then(result => {
-        console.log(result);
-        res.redirect(`/listings/${id}`)})
-        .catch(err=>{
-        res.send(`Problem updating the listing : ${err}`);
-    });
-});
+    await Listing.findByIdAndUpdate(id, edited, {new: true})
+    
+        res.redirect(`/listings/${id}`)
+    }));
 
 
-//Delete
-
-app.delete("/listings/:id", async(req, res)=>{
+app.delete("/listings/:id", wrapAsync(async(req, res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndDelete(id).then(res.redirect("/listings"));
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 
+}) );
+
+app.use((req, res, next) => {
+    next(new ExpressErrors(404, "Page Not Found! Error 404"));
 });
 
 
+app.use((err, req, res, next)=>{
+    let {statusCode=500, message="Something went wrong!"} = err;
+    // res.status(statusCode).send(message);
 
+    res.render("./errors/error.ejs", {statusCode, message});
+});
+
+app.listen(port, (req, res)=>{console.log(`listening to port 3000`)});
---
+++ utils/ExpressErrors.js
@@ -0,0 +1,8 @@
+class ExpressErrors extends Error{
+    constructor(statusCode, message){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ExpressErrors;
