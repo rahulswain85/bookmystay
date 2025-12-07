28ec16baf2f8690176e1d7cfa3efe67244731e68
rahulswain85 <rahul8033swain@gmail.com>
2025-12-07 08:29:19 +0530

    temp folder init

--- init/index.js
+++ init/index.js
@@ -1,27 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-const initData = require('./data.js');
-const Listing = require('../models/listing.js');
 
-const mongo_url = "mongodb://127.0.0.1:27017/wanderlust"
+// 1. Ensure dotenv is configured before using the variable
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-main().then(()=>{console.log("connected to db")}).catch(err=>{console.log(err)});
-async function main(){
-    await mongoose.connect(mongo_url);
+// 2. Update this line to use the Atlas URL
+// OLD: const mongo_url = "mongodb://127.0.0.1:27017/wanderlust"
+const mongo_url = process.env.MONGO_URI;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(mongo_url);
 }
 
+const initializeDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    // Note: Ensure this Owner ID exists in your new Atlas User collection
+    owner: "693474a07cab92cdb89dfbaa",
+  }));
 
-const initializeDB = async ()=>{
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({
-      ...obj,
-      owner: "68f271415b18b079708d4a59",
-    }));
-
-    await Listing.insertMany(initData.data);
-
-    console.log('Data was Initialized!');
+  await Listing.insertMany(initData.data);
+  console.log("Data was Initialized!");
 };
 
 initializeDB();
-
